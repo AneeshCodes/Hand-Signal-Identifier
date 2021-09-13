66644b842c95095b0811dab942b38b30a0ce529b
AneeshCodes <74843895+AneeshCodes@users.noreply.github.com>
2021-09-12 17:31:53 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function takesnapshot()
 
 console.log('ml5 version:',ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1ob50bhm4/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/PFuyZnzId/model.json', modelLoaded);
 
 function modelLoaded()
 {
@@ -30,10 +30,46 @@ function modelLoaded()
 
 function Speak()
 {
-  synth = window.SpeechSynthesis;
+  var synth = window.speechSynthesis;
   sd1 = "The first prediction is " + p1;
   sd2 = "and the second prediction is " + p2;
   utterThis = new SpeechSynthesisUtterance(sd1 + sd2);
   synth.speak(utterThis);
 }
 
+function check()
+{
+  img = document.getElementById('captured_image');
+  classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+  if(error)
+  {
+    console.log(error);
+  }
+  else
+  {
+    console.log(results);
+    p1 = results[0].label;
+    p2 = results[1].label;
+    document.getElementById('result_emotion_name').innerHTML = p1;
+    
+
+    if(p1 == "Thumbs Up")
+    {
+      document.getElementById('update_emoji').innerHTML = "&#128077";
+    }
+    if(p1 == "Thumbs Down")
+    {
+      document.getElementById('update_emoji').innerHTML = "&#128078";
+    }
+    if(p1 == "Victory")
+    {
+      document.getElementById('update_emoji').innerHTML = "&#9996";
+    }
+
+    speak()
+  }
+}
